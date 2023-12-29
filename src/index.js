9930eb8ea4fd0f7fb622dcc45b1852a2800102b8
Rohit <rsaw409@gmail.com>
2023-12-29 10:22:06 +0530

    merge tictoe backend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import express from "express";
 
 import connectToPostgres from "../src/db/postgres.js";
 import { addRoutes } from "./app.js";
+import { addSocket } from "./socket.js";
 
 const main = async () => {
   const app = express();
 
+  app.use("/health", (req, res) =>
+    res.status(200).send({ message: "Server is Running." })
+  );
+
+  // Backend for https://portfolio-rsaw409.onrender.com/projects
   addRoutes(app);
+
+  // Backend For https://tictoe-rsaw409.onrender.com
+  addSocket(app);
 
   await connectToPostgres({ logging: true });
 };
---
+++ src/socket.js
@@ -0,0 +1,107 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = 3001;
+
+const addSocket = (app) => {
+  const http = createServer(app);
+
+  const games = {};
+
+  app.use("/currentGames", (req, res) => {
+    res.status(200).send({ games: games });
+  });
+
+  const io = new Server(http, {
+    cors: {
+      origin:
+        process.env.NODE_ENV === "production"
+          ? "https://tictoe-rsaw409.onrender.com"
+          : "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", function (socket) {
+    socket.on("joinGame", (data) => {
+      const gameId = data.gameId;
+      const userName = data.userName;
+      const userId = socket.id;
+
+      let users = io.sockets.adapter.rooms.get(gameId);
+      if (!users || users?.size < 2) {
+        socket.join(gameId);
+        console.log(
+          `${userName} - ${userId} has joined game with id ${gameId}`
+        );
+
+        if (!games.hasOwnProperty(gameId)) games[gameId] = {};
+        games[gameId][userId] = userName;
+      }
+
+      users = io.sockets.adapter.rooms.get(gameId);
+
+      if (users?.size === 2) {
+        io.sockets.in(gameId).emit("users", games[gameId]);
+      }
+    });
+
+    socket.on("madeMove", (data) => {
+      const gameId = data.gameId;
+      const userName = data.userName;
+      const [i, j] = data.index;
+      const symbol = data.symbol;
+      console.log(`${userName} has made his move.`);
+      socket.to(gameId).emit("receivedFromServer", data);
+    });
+
+    socket.on("RestartGame", (data) => {
+      socket.to(data.gameId).emit("userRestartedGame", data);
+    });
+
+    socket.on("LeftGame", (data) => {
+      socket.leave(data.gameId);
+
+      try {
+        delete games?.[data?.gameId]?.[data?.userId];
+        if (Object.keys(games?.[data?.gameId] ?? {})?.length === 0) {
+          delete games?.[data?.gameId];
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      socket.to(data.gameId).emit("userLeftGame", data);
+    });
+
+    socket.on("disconnecting", (reason) => {
+      for (const room of socket.rooms) {
+        if (room !== socket.id) {
+          let userName = "";
+          try {
+            userName = games?.[room]?.[socket.id];
+            delete games?.[room]?.[socket.id];
+            if (Object.keys(games?.[room] ?? {}).length === 0) {
+              delete games?.[room];
+            }
+          } catch (error) {
+            console.error(error);
+          }
+
+          socket.to(room).emit("userDisconnected", userName);
+        }
+      }
+    });
+  });
+
+  http.listen(PORT, (error) => {
+    if (!error) {
+      console.log(
+        "Socket Server is Successfully Running, and App is listening on port " + PORT
+      );
+    } else {
+      console.log("Error occurred, server can't start", error);
+    }
+  });
+};
+
+export { addSocket };
