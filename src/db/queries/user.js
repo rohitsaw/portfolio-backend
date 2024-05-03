64f1df139b126c0cf0dcd0b760301c4191592371
Rohit <rsaw409@gmail.com>
2024-05-03 22:21:06 +0530

    Bug fix in update user

--- src/db/queries/user.js
+++ src/db/queries/user.js
@@ -9,17 +9,20 @@ const getuser = async (email) => {
   return res;
 };
 
-const addOrUpdateUser = async ({ user_email, name, about, social_links }) => {
+const addOrUpdateUser = async (
+  { user_email, name, about, social_links },
+  user_id
+) => {
   social_links = JSON.stringify(social_links);
   const query = `update users set
     name = :name, 
     about = :about,
     social_links = :social_links
-    where user_email = :user_email
+    where user_email = :user_email and id = :user_id
     `;
   const res = await psql.query(query, {
     type: psql.QueryTypes.SELECT,
-    replacements: { user_email, name, about, social_links },
+    replacements: { user_email, name, about, social_links, user_id },
   });
   return res;
 };
