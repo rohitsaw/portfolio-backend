9679e7b30b4981c899cb98c88a52c55bcbe0e3dd
Rohit <rsaw409@gmail.com>
2024-08-22 10:03:26 +0530

    add logs

--- src/split-backend/controller.js
+++ src/split-backend/controller.js
@@ -39,8 +39,10 @@ const joinGroup = async (req, res) => {
 
     console.log("inviteId", req.body.invite_id);
     const group_id = decrypt(req.body.invite_id);
+    console.log("decrypted group id", group_id);
 
     const response = await getGroupInDB({ group_id: group_id });
+    console.log("group response", response);
 
     if (response == null) {
       throw new Error("No Group Found");
