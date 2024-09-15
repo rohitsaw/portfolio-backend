d11ae870ffea4a2107682640cc7c63a3a1e0854e
Rohit <rsaw409@gmail.com>
2024-09-15 13:11:33 +0530

    add unit tests

---
+++ src/split-backend/utils/crypto.test.ts
@@ -0,0 +1,14 @@
+import { encrypt, decrypt } from './crypto';
+
+describe('CRYPTO TESTS', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    process.env.ENCRYPTION_KEY = 'tests';
+    process.env.IV = 'dsjdw';
+  });
+
+  test('encrypt and descrypt test', () => {
+    const text: string = 'test Data';
+    expect(decrypt(encrypt(text))).toEqual(text);
+  });
+});
---
+++ src/split-backend/utils/send_notification.test.ts
@@ -0,0 +1,31 @@
+import { send_push_notification } from './send_notification';
+
+jest.mock('node-fetch', () => ({
+  __esModule: true,
+  default: jest.fn((url, options) => {
+    expect(url).toEqual('https://onesignal.com/api/v1/notifications');
+    expect(options).toHaveProperty('method', 'POST');
+    expect(options).toHaveProperty('headers');
+    expect(options).toHaveProperty('body');
+    return Promise.resolve({
+      json: () => Promise.resolve({ message: 'Mocked response' }),
+      status: 200,
+    } as Response);
+  }),
+}));
+
+describe('SEND NOTIFCATION TEST', () => {
+  beforeEach(() => {
+    process.env.ONESIGNAL_KEY = 'test-key';
+  });
+
+  test('send notification test', async () => {
+    const { default: fetch } = await import('node-fetch');
+    send_push_notification({
+      groupName: 'testGroup',
+      headings: 'test heading',
+      title: 'test title',
+    });
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
